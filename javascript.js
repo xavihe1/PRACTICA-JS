6642dfe92d7835b7dc7d18429857bc0cba2286d9
xavihe1 <146827255+xavihe1@users.noreply.github.com>
2023-11-10 08:13:43 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,128 +1,61 @@
-const iteracionsMax = 10;
-let numSecreto = generateSecretNum();
-let iteracions = 1;
-
-function generateSecretNum() {
-    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let secretNum = '';
-
-    for (let i = 0; i < 5; i++) {
-        let randomNum = Math.floor(Math.random() * nums.length);
-        secretNum += nums.splice(randomNum, 1)[0];
-    }
-    return secretNum;
+//Generem el numero de 5 digits
+function generarNumeroAleatorio() {
+    return Math.floor(10000 + Math.random() * 90000);
 }
 
-const cartaElements = document.querySelectorAll('.carta');
 
-if (numSecreto.length === 5 && cartaElements.length >= 5) {
-    for (let i = 0; i < 5; i++) {
-        cartaElements[i].textContent = numSecreto[i];
-    }
-}
+var numeroSecreto = generarNumeroAleatorio().toString();
+var intentosRestantes = 5;
 
-function intentosRestantes() {
-    const intentosRestantesElement = document.getElementById('intentosRestantes');
-    const remainingAttempts = iteracionsMax - iteracions;
-    intentosRestantesElement.textContent = remainingAttempts;
-}
-
-function comprobarNum() {
-    const inputElement = document.getElementById('01234');
-    const inputValue = inputElement.value;
-
-    if (inputValue.length === 5) {
-        if (inputValue === numSecreto) {
-            document.getElementById('informacion').innerHTML = '¡Has adivinado el número!';
-        } else {
-            document.getElementById('informacion').innerHTML = 'Incorrecto, sigue intentando.';
-        }
-    } else {
-        document.getElementById('informacion').innerHTML = 'Ingrese un número de 5 dígitos';
-    }
-
-    iteracions++
-    intentosRestantes();
-//Generem un numero aleatori de 5 digits
-function generarNumAleatori() {
-    let numeroAleatori = "";
-    for (let i = 0; i < 5; i++) {
-        numeroAleatori += Math.floor(Math.random() * 10);
-    }
-    return numeroAleatori;
-}
-
-//Iniciem les variables 
-let numeroAleatori = generarNumAleatori();
-let intentosRestantes = 5;
-
-//Comprovem el numero que ha posat l'usuari
+//Comprovem el nombre ingresat per l'usuari
 function comprovarNum() {
-    const inputElement = document.getElementById("01234");
-    const numeroUsuari = inputElement.value;
-
-    if (numeroUsuari.length !== 5 || isNaN(numeroUsuari)) {
-        alert("Por favor, ingresa un número de 5 dígitos.");
+    var inputNumero = document.getElementById('01234').value;
+    
+    if (inputNumero.length !== 5 || isNaN(inputNumero)) {
+        alert('Por favor, introduce un número de 5 dígitos.');
         return;
     }
 
-
-    compararNumeros(numeroUsuari);
-
-    //Reduim el nombre d'intents en 1 per cada cop que l'usuari ingresa un numero
+    //Restem intets
     intentosRestantes--;
 
+    
+    document.getElementById('intentosRestantes').textContent = intentosRestantes;
 
-    document.getElementById("intentosRestantes").innerText = intentosRestantes;
-
-
+    
     if (intentosRestantes === 0) {
-        alert("¡Has agotado tus intentos! El número secreto era " + numeroAleatori + ".");
-        reiniciarJuego();
+        alert('¡Se acabaron los intentos! El número secreto era: ' + numeroSecreto);
+        location.reload(); 
+        return;
     }
-}
 
-//Comparem els numeros i actualitzem el resultat
-function compararNumeros(numeroUsuari) {
-    const resultadoElement = document.getElementById("resultado");
+    
+    var resultadoDiv = document.getElementById('resultado');
+    var filaDiv = document.createElement('div');
+    filaDiv.className = 'result';
 
-    const filaResultado = document.createElement("div");
-    filaResultado.className = "result";
+    
+    for (var i = 0; i < 5; i++) {
+        var celdaDiv = document.createElement('div');
+        celdaDiv.className = 'result-cube';
 
-    for (let i = 0; i < 5; i++) {
-        const resultCube = document.createElement("div");
-        resultCube.className = "result-cube";
-
-        if (numeroUsuari[i] === numeroAleatori[i]) {
-            resultCube.style.backgroundColor = "green";
-        } else if (numeroAleatori.includes(numeroUsuari[i])) {
-            resultCube.style.backgroundColor = "yellow";
+        
+        if (inputNumero[i] === numeroSecreto[i]) {
+            celdaDiv.style.backgroundColor = 'green';
+        } else if (numeroSecreto.includes(inputNumero[i])) {
+            celdaDiv.style.backgroundColor = 'yellow';
         } else {
-            resultCube.style.backgroundColor = "gray";
+            celdaDiv.style.backgroundColor = 'gray';
         }
 
-        filaResultado.appendChild(resultCube);
+        
+        celdaDiv.textContent = inputNumero[i];
+        filaDiv.appendChild(celdaDiv);
     }
 
-    resultadoElement.appendChild(filaResultado);
-
-    //Verifiquem si s'ha encertat el numero aleatori
-    if (numeroUsuari === numeroAleatori) {
-        alert("¡Felicidades! Has adivinado el número secreto.");
-        reiniciarJuego();
-    }
+    
+    resultadoDiv.appendChild(filaDiv);
 }
 
-//Reiniciem el joc
-function reiniciarJuego() {
-    numeroAleatori = generarNumAleatori();
-    intentosRestantes = 5;
-    document.getElementById("intentosRestantes").innerText = intentosRestantes;
 
-    //Esborrem el resultat anterior
-    const resultadoElement = document.getElementById("resultado");
-    while (resultadoElement.firstChild) {
-        resultadoElement.removeChild(resultadoElement.firstChild);
-    }
-}
-}
+document.getElementById('intentosRestantes').textContent = intentosRestantes;
